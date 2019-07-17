d1603a72de3cf99b5d9ca5d44609043c0d9c5821
Rob Balfre <robert.balfre@travelctm.com.au>
2019-07-17 20:14:02 +1000

    init

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import SlotTest from 'slot-test';
+	import Baz from './Baz.svelte';
+
+	let bookings = ['one', 'two', 'three'];
+
+	function handleEmpty() {
+		bookings = []
+	}
+</script>
+
+<button on:click={handleEmpty}>
+	Empty bookings
+</button>
+
+{#each bookings as booking}
+<SlotTest>
+	<Baz></Baz>
+</SlotTest>
+{/each}
